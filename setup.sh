0e9f4b7880c1a8a7e91963a6272eae06b8dcf963
DetectiveBatman <sinamedic1@gmail.com>
2019-09-06 03:59:46 +0430

    Added set-up file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt install git
+git clone https://github.com/DetectiveBatman/ComaStudio.git
+cd ComaStudio
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+nvm install 12.0.0
+nvm use 12.0.0
+nvm install-latest-npm
+npm install
