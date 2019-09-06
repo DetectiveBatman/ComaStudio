c2d345fbda28068f885da44df5fd202da7bc9f47
DetectiveBatman <sinamedic1@gmail.com>
2019-09-07 00:55:07 +0430

    Stopped using NVM

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 sudo apt-get update
 sudo apt install git
+sudo apt install curl
 git clone https://github.com/DetectiveBatman/ComaStudio.git
 cd ComaStudio
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-nvm install 12.0.0
-nvm use 12.0.0
-nvm install-latest-npm
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
 npm install
 npm start
