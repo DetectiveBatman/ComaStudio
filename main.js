b0c587f2e90aca59f78c9d093b57f96859af77d5
root <root@berbidvps.4.2.2.4>
2019-09-12 14:08:04 -0400

    Now it supports php! We can use phpmyadmin!

--- main.js
+++ main.js
@@ -3,7 +3,19 @@ const express = require('express');
 const chalk = require('chalk');
 const app = express();
 const args = process.argv.splice(2);
-const port = args[0] || 8000;
+const port = args[0] || 8545;
+
+var execPHP = require('./execphp.js')();
+
+execPHP.phpFolder = '/var/www/html/';
+
+app.use('*.php',function(request,response,next) {
+	execPHP.parseFile(request.originalUrl,function(phpResult) {
+		response.write(phpResult);
+		response.end();
+	});
+});
+
 
 app.use(express.static(__dirname));
 
