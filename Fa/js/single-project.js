d9bcf812f9e13f276f7dd84dc94db6fa14fab3c8
DetectiveBatman <sinamedic1@gmail.com>
2019-10-06 18:19:57 +0330

    it now updates page based on the request

--- Fa/js/single-project.js
+++ Fa/js/single-project.js
@@ -6,12 +6,18 @@ $(document).ready(() => {
   $.post('/api/getPortfolio', {id: id}, (response) => {
     if (response.ok == 'true') {
       var item = response.res[0];
-      let img = `../lib/assets/${item.img}.jpg`;
+      let img = `../lib/assets/${item.largeImg}.jpg`;
       let category = item.category;
       let subcat = item.subcat;
       let title = item.title;
+      let description = item.description;
 
-
+      document.title = title + ' - کما استودیو';
+      $("#top-bar-title").text(title);
+      $("#top-bar-prtitle").text(title);
+      $("#project-title").text(title);
+      $("#project-image").attr('src', img);
+      $("#project-description").text(description);
     } else {
       alert('ناموفق بود...')
     }
