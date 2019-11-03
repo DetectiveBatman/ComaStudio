7a869401840340c6733d54133769748421cbf5cc
DetectiveBatman <sinamedic1@gmail.com>
2019-11-03 20:20:19 +0330

    handles requests for specific categories

--- api/main.js
+++ api/main.js
@@ -1,6 +1,15 @@
 module.exports = function launchAPI(app, db) {
     app.post('/api/getPortfolio', (req, res, next) => {
-      let query = req.body.id ? `SELECT * FROM portfolio WHERE id=${req.body.id}` : `SELECT * FROM portfolio`;
+
+      if (req.body.id) {
+        let query = `SELECT * FROM portfolio WHERE id=${req.body.id}`;
+      } else if (req.body.category) {
+        let query = `SELECT * FROM portfolio WHERE category=${req.body.category}`
+      }
+      else {
+        let query = `SELECT * FROM portfolio`;
+      }
+
       db.query(query, (err, resp, fld) => {
         if (err) console.error(err);
 
