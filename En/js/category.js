d052d212328bb4fd3786d64d29cd3346122c035f
DetectiveBatman <sinamedic1@gmail.com>
2019-12-16 12:26:35 +0330

    fixture

--- En/js/category.js
+++ En/js/category.js
@@ -12,7 +12,7 @@ function showSub(sub) {
   $("#category-photo-1").attr('src', `../lib/assets/${photoName[1]}`);
   if (sub != "all") {
     $("#category-photo-2").attr('src', `../lib/assets/${photoName[0]}`);
-    $("#subcat-portfolio").attr('href', `/Fa/subcategory?name=${sub}`);
+    $("#subcat-portfolio").attr('href', `/En/subcategory?name=${sub}`);
     $("#portfolio-show").css('display', 'block');
     $("#category-photo-sec").css('display', 'none');
   } else {
