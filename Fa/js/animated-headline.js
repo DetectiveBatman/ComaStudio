2af4f6c29053fca2977c8695f9366e45803a1184
DetectiveBatman <sinamedic1@gmail.com>
2019-12-17 12:36:56 +0330

    fixed top animation

--- Fa/js/animated-headline.js
+++ Fa/js/animated-headline.js
@@ -1,13 +1,13 @@
-var count = 2;
-var i = 0;
+var count = $('.top-animation b').length;
+var i = 1;
 function changeHeader(i){
-	$("#top-headline-" + i).fadeOut('fast', () => {
-		i = (i + 1) % count;
-		$("#top-headline-" + i).fadeIn('fast');
+	$(`.top-animation b:nth-child(${i})`).fadeOut('fast', () => {
+		i = (i + 1) % count == 0 ? count : (i + 1) % count;
+		$(`.top-animation b:nth-child(${i})`).fadeIn('fast');
 	});
 	return i;
 }
 
 setInterval(() => {
 	i = changeHeader(i);
-}, 3000);
+}, 1000);
