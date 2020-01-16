4c6cb423a620b3da6c10750a169e1c21370d7472
DetectiveBatman <sinamedic1@gmail.com>
2020-01-16 09:51:15 +0330

    fixed disconnetion

--- config.js
+++ config.js
@@ -9,35 +9,17 @@ var connection = mysql.createConnection({
   multipleStatements: true
 });
 
-function handleDisconnect(connection){
-  connection.on('error', function(err){
-    if(!err.fatal)
-    {
-      return;
-    }
-    if(err.code !== 'PROTOCOL_CONNECTION_LOST')
-    {
-      throw err;
-    }
-    console.log('\nRe-connecting lost connection: ' +err.stack);
-
-    connection = mysql.createConnection(connection.config);
+function handleDisconnect(myconnection) {
+  myconnection.on('error', function(err) {
+    console.log('Re-connecting lost connection');
+    connection.destroy();
+    connection = mysql.createConnection(config.mysql);
     handleDisconnect(connection);
     connection.connect();
   });
 }
 
 handleDisconnect(connection);
-
-
-
-
-/** db.connect(function(err) {
-  if (err) {
-    console.error(chalk.red('error connecting to database [ComaStudio]: ') + chalk.green(err.stack));
-    return;
-  }
-}); **/
 
 module.exports = {
   db: connection
