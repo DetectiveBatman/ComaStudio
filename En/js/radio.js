9bc2fcdfcd958edcc834ae7972e03bea90dfbd44
DetectiveBatman <sinamedic1@gmail.com>
2020-02-25 18:29:48 +0330

    English side's radio page updated

--- En/js/radio.js
+++ En/js/radio.js
@@ -1,20 +1,36 @@
 $(document).ready(() => {
   $.post('/api/getRadio', (response) => {
     if (response.ok == 'true'){
-      let podcast = response.res[0];
-      let file = podcast.file;
-      let description = podcast.enDescription;
-      $("#radio-description").text(description);
-      let audio = `
-      <div id="audio-div">
-      <audio id="audio-player" preload="auto" controls>
-          <source id="audio-file" src="../lib/radio/${file}" type="audio/mpeg">
-          Your browser doesn't support this feature. Use another browser.
-      </audio>
-      </div>`;
-      $(".main-container").append(audio);
+      let podcast_items = response.res;
+      for (let i = 0; i < podcast_items.length; i++) {
+        let podcast = podcast_items[i];
+
+        let file          = podcast.file;
+        let title         = podcast.title;
+        let enTitle       = podcast.enTitle;
+        let description   = podcast.description;
+        let enDescription = podcast.enDescription;
+
+        let element = `
+        <li>
+          <div class="podcast-name">
+            <span>${enTitle}</span>
+          </div>
+
+          <p>${enDescription}</p>
+          <div class="audio-div">
+          <audio class="audio-player" preload="auto" controls>
+              <source class="audio-file" src="../lib/radio/${file}" type="audio/mpeg">
+              Your browser doesn't support this feature. Use another browser.
+          </audio>
+          </div>
+        </li>
+        `;
+
+        $("#podcast-ul").append(element);
+      }
     } else {
-      alert('Error');
+      alert('Error!');
     }
   });
 });
